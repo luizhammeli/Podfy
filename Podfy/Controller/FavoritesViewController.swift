ca79f9179e9fa988fa9a669486ec20417eafb9de
Luiz Hammerli <luiz@whitebook.com.br>
2018-05-22 17:45:52 -0300

    **1 - Descrição do que foi implementado:**
    **2 - Arquivos/Diretórios comitados (ou pastas, ou algo que esclareça o que foi alterado, se necessário):**
    
    **3 - Locais comitados:**
    
    **4 - Pontos que podem ser afetados pelas alterações realizadas:**
    
    **5 - FTP foi alterado ? (S/N)**

--- Podfy/Controller/FavoritesViewController.swift
+++ Podfy/Controller/FavoritesViewController.swift
@@ -9,10 +9,17 @@
 import UIKit
 import FirebaseAuth
 
-class FavoritesViewController: UIViewController{
+class FavoritesViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout{
+
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+    let cellID = "cellID"
     
     override func viewDidLoad() {
-        super.viewDidLoad()        
+        super.viewDidLoad()
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        collectionView.backgroundColor = .black
     }
     
     @IBAction func logOut(_ sender: Any) {
@@ -22,5 +29,31 @@ class FavoritesViewController: UIViewController{
             self.dismiss(animated: true, completion: nil)
         }))
         self.present(alertController, animated: true, completion: nil)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = self.collectionView.dequeueReusableCell(withReuseIdentifier: cellID, for: indexPath)
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let size = (self.view.frame.width-50)/2
+        return CGSize(width: size, height: size+50)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return UIEdgeInsets(top: 10, left: 14, bottom: 10, right: 14)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 22
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 22
     }
 }
